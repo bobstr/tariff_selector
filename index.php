ddc146520ac6accf9d678da6e263dca8ad55bc5b
bobstr <rob.tsyura@gmail.com>
2019-12-12 00:44:25 +0300

    layout main page

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/reset.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>SkyNet's test task (tariffs)</title>
+</head>
+<body>
+<main>
+
+<section class="tariff">
+    <div class="top">
+        <h2 class="title">Тариф "Земля"</h2>
+    </div>
+    <div class="content">
+        <a class="content_link" href="">
+            <div class="speed bg-brown">
+                <span class="speed_count">50</span>
+                <span class="speed_measurement">Мбит/с</span>
+            </div>
+            <div class="price">
+                <span class="price_count">350 - 480</span>
+                <span class="price_measurement">&#8381/мес</span>
+            </div>
+        </a>
+    </div>
+    <div class="bottom">
+        <a href="https://www.sknt.ru/tarif-internet/in/tarif-internet-zemlya.htm">узнать подробнее на сайте www.sknt.ru</a>
+    </div>
+</section>
+
+<section class="tariff">
+    <div class="top">
+        <h2 class="title">Тариф "Вода"</h2>
+    </div>
+    <div class="content">
+        <a class="content_link" href="">
+            <div class="speed bg-blue">
+                <span class="speed_count">100</span>
+                <span class="speed_measurement">Мбит/с</span>
+            </div>
+            <div class="price">
+                <span class="price_count">450 - 600</span>
+                <span class="price_measurement">&#8381/мес</span>
+            </div>
+            <div class="package">
+                <ul class="package_list">
+                    <li>ТВ пакет "Социальный ТВ"</li>
+                    <li>Антивирус Agnitum Outpost</li>
+                </ul>
+            </div>
+        </a>
+    </div>
+    <div class="bottom">
+        <a href="https://www.sknt.ru/tarif-internet/in/tarif-internet-zemlya.htm">узнать подробнее на сайте www.sknt.ru</a>
+    </div>
+</section>
+
+<section class="tariff">
+    <div class="top">
+        <h2 class="title">Тариф "Огонь"</h2>
+    </div>
+    <div class="content">
+        <a class="content_link" href="">
+            <div class="speed bg-orange">
+                <span class="speed_count">200</span>
+                <span class="speed_measurement">Мбит/с</span>
+            </div>
+            <div class="price">
+                <span class="price_count">650 - 800</span>
+                <span class="price_measurement">&#8381/мес</span>
+            </div>
+            <div class="package">
+                <ul class="package_list">
+                    <li>ТВ пакет "Социальный ТВ"</li>
+                    <li>Антивирус Agnitum Outpost</li>
+                    <li>Доменное имя в зоне sknt.ru</li>
+                </ul>
+            </div>
+        </a>
+    </div>
+    <div class="bottom">
+        <a href="https://www.sknt.ru/tarif-internet/in/tarif-internet-zemlya.htm">узнать подробнее на сайте www.sknt.ru</a>
+    </div>
+</section>
+
+</main>
+</body>
+</html>
